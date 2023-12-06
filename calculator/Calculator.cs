334fadea50457a41c9cef5b425a8f6bb14bda625
sam98963 <szwilliamson01@gmail.com>
2023-12-06 11:40:03 +0000

    something runs

---
+++ calculator/Calculator.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Calculator{
+  static void Main(){
+    Console.Write("Enter a number: ");
+    string userInput = Console.ReadLine();
+    Console.WriteLine("You entered: " + userInput);
+  }
+}
