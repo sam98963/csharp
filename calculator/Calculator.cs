0ee456fd37fa2d172241800e984b15938dad84e2
sam98963 <szwilliamson01@gmail.com>
2023-12-06 11:46:21 +0000

    3 inputs, now to make a switch case

--- calculator/Calculator.cs
+++ calculator/Calculator.cs
@@ -3,7 +3,15 @@ using System;
 class Calculator{
   static void Main(){
     Console.Write("Enter a number: ");
-    string userInput = Console.ReadLine();
-    Console.WriteLine("You entered: " + userInput);
+    string firstInput = Console.ReadLine();
+    Console.Write("Enter another number: ");
+    string secondInput = Console.ReadLine();
+    Console.Write("Now give an operator: +, -, * or / ");
+    string operatorInput = Console.ReadLine();
+    Console.WriteLine(calculate(firstInput, secondInput, operatorInput));
+  }
+
+  static string calculate(string firstInput, string secondInput, string operatorInput){
+    return("You entered: " + firstInput + " " + secondInput+ " " + operatorInput);
   }
 }
