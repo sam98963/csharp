71ba6b761e2adbede6e64f9638bf60d17be45018
sam98963 <szwilliamson01@gmail.com>
2023-12-06 11:58:53 +0000

    seperated into methods and complete

--- calculator/Calculator.cs
+++ calculator/Calculator.cs
@@ -1,17 +1,50 @@
 using System;
 
 class Calculator{
+  private double firstInput;
+  private double secondInput;
+  private string operatorInput;
+  private double result;
+
   static void Main(){
-    Console.Write("Enter a number: ");
-    string firstInput = Console.ReadLine();
-    Console.Write("Enter another number: ");
-    string secondInput = Console.ReadLine();
-    Console.Write("Now give an operator: +, -, * or / ");
-    string operatorInput = Console.ReadLine();
-    Console.WriteLine(calculate(firstInput, secondInput, operatorInput));
+    Calculator calculator = new Calculator();
+    calculator.GetInputs();
+    Console.WriteLine(calculator.Calculate());
   }
 
-  static string calculate(string firstInput, string secondInput, string operatorInput){
-    return("You entered: " + firstInput + " " + secondInput+ " " + operatorInput);
+    public Calculator(){
+    this.firstInput = 0;
+    this.secondInput = 0;
+    this.operatorInput = "";
+    this.result = 0.0;
+    }
+
+
+  void GetInputs(){
+    Console.Write("Enter a number: ");
+    this.firstInput = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter another number: ");
+    this.secondInput = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Now give an operator: +, -, * or / ");
+    this.operatorInput = Console.ReadLine();
+  }
+
+   double Calculate(){
+    switch (this.operatorInput)
+    {
+      case "+":
+      this.result = this.firstInput + this.secondInput;
+      break;
+      case "-":
+      this.result = this.firstInput - this.secondInput;
+      break;
+      case "*":
+      this.result = this.firstInput * this.secondInput;
+      break;
+      case "/":
+      this.result = this.firstInput / this.secondInput;
+      break;
+    }
+    return(result);
   }
 }
