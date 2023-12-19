4d878fa7dd9adf0ef13176dab538f6ebf56209db
sam98963 <szwilliamson01@gmail.com>
2023-12-19 12:44:02 +0000

    complete

--- unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -5,13 +5,39 @@ namespace Prime.UnitTests.Services
 {
     public class PrimeService_IsPrimeShould
     {
-        [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
-        {
-            var primeService = new PrimeService();
-            bool result = primeService.IsPrime(1);
+      private readonly PrimeService _primeService;
+      public PrimeService_IsPrimeShould()
+      {
+          _primeService = new PrimeService();
+      }
+      [Theory]
+      [InlineData(-1)]
+      [InlineData(0)]
+      [InlineData(1)]
+      public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+      {
+          var result = _primeService.IsPrime(value);
 
-            Assert.False(result, "1 should not be prime");
-        }
+          Assert.False(result, $"{value} should not be prime");
+      }
+
+      [Theory]
+      [InlineData(4)]
+      [InlineData(6)]
+      [InlineData(12)]
+      public void IsPrime_ValueDivisibleBeforeSqrt(int value)
+      {
+          var result = _primeService.IsPrime(value);
+          Assert.False(result, $"{value} should not be prime");
+      }
+      [Theory]
+      [InlineData(3)]
+      [InlineData(7)]
+      [InlineData(11)]
+      public void IsPrime_ValueIsPrime(int value)
+      {
+          var result = _primeService.IsPrime(value);
+          Assert.True(result, $"{value} should be prime");
+      }
     }
 }
