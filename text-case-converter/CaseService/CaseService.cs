8e41aa5b96e6cc35b73ab5516f23bbb56d72a569
sam98963 <szwilliamson01@gmail.com>
2023-12-19 16:01:50 +0000

    case converter

---
+++ text-case-converter/CaseService/CaseService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Case.Services{
+  public class CaseService{
+    public string capitaliseFirst(string input){
+      return input[0].ToString().ToUpper() + input.Substring(1);
+    }
+  }
+}
