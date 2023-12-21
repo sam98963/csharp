f12bb60161a5d26f0add98589910c006baaecc9d
sam98963 <szwilliamson01@gmail.com>
2023-12-21 09:20:10 +0000

    another test complete

--- text-case-converter/CaseService/CaseService.cs
+++ text-case-converter/CaseService/CaseService.cs
@@ -4,5 +4,13 @@ namespace Case.Services{
     public string capitaliseFirst(string input){
       return input[0].ToString().ToUpper() + input.Substring(1);
     }
+
+    public string splitStrings(string input){
+      string[] words = input.Split(" ");
+      for (int i = 0; i<words.Length; i++){
+        words[i] = capitaliseFirst(words[i]);
+      }
+      return string.Join(" ", words);
+    }
   }
 }
